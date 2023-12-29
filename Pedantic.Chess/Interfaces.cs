d2a6508dffd70c527fe529221bc0cbf128420044
JoAnnP38 <JoAnn.Peeler@gmail.com>
2023-12-29 14:46:59 -0500

    Use inline array in MoveList instead of allocated array from heap. Score of Pedantic Dev vs Pedantic Base: 3754 - 3647 - 7297  [0.504] 14698 ...      Pedantic Dev playing White: 2203 - 1494 - 3652  [0.548] 7349 ...      Pedantic Dev playing Black: 1551 - 2153 - 3645  [0.459] 7349 ...      White vs Black: 4356 - 3045 - 7297  [0.545] 14698 Elo difference: 2.5 +/- 4.0, LOS: 89.3 %, DrawRatio: 49.6 % SPRT: llr 2.2 (100.0%), lbound -2.2, ubound 2.2 - H1 was accepted info string depth 15 time 18.6020 nodes 28760079 nps 1546074.5619

--- Pedantic.Chess/Interfaces.cs
+++ Pedantic.Chess/Interfaces.cs
@@ -1,8 +1,23 @@
-﻿namespace Pedantic.Chess
+﻿using System.Collections;
+
+namespace Pedantic.Chess
 {
     public interface IHistory
     {
         public short this[Color stm, Piece piece, int to] { get; }
         public short this[ulong move] { get; }
+    }
+
+    public interface IMoveList : IEnumerable<ulong>, IEnumerable
+    {
+        public ulong this[int index] { get; }
+        public void Add(ulong move);
+        public void Add(Color stm, Piece piece, int from, int to, MoveType type, Piece capture, Piece promote, int score);
+        public void Add(IEnumerable<ulong> moves);
+        public ReadOnlySpan<ulong> AsSpan();
+        public void Clear();
+        public int Count { get; }
+        public ulong Sort(int n);
+        public bool Remove(ulong move);
     }
 }
